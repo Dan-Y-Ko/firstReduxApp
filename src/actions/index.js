4cb240c0fda192e42ad62351d7ee7181b6a9c4d0
Dan-Y-Ko <dk3790@hotmail.com>
2018-08-07 16:58:30 -0400

    finished

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 export function selectBook(book)
 {
-    // selectBook is an ActionCreator, it needs to retur and action,
+    // selectBook is an ActionCreator, it needs to return an action,
     // an object with a type property
     return {
         type: 'BOOK_SELECTED',
